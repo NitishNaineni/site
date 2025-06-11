40c83e984b26484e0d98186462dd0592344d99cd
Nitish Naineni <naineninitish@gmail.com>
2025-06-11 14:52:32 -0500

    upadated to use mathjax instead of katex

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,10 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const markdownIt = require("markdown-it");
-const markdownItKatex = require("markdown-it-katex");
 
 module.exports = function (eleventyConfig) {
   // --- Plugins ---
+  // We only need the syntax highlighter now.
   eleventyConfig.addPlugin(syntaxHighlight);
-
-  // --- Custom Markdown Library ---
-  const md = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-  }).use(markdownItKatex);
-  eleventyConfig.setLibrary("md", md);
 
   // --- Passthrough Copies ---
   eleventyConfig.addPassthroughCopy("css");
@@ -22,14 +13,12 @@ module.exports = function (eleventyConfig) {
 
   // --- Date Formatting Filters ---
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    // Correct format: "Month Day, Year" e.g., "June 11, 2025"
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "LLLL d, yyyy",
     );
   });
 
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    // Correct format: "YYYY-MM-DD" e.g., "2025-06-11"
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
@@ -40,6 +29,8 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       output: "_site",
     },
+    // We can rely on Eleventy's default Markdown processor
+    // and let MathJax handle the math in the browser.
     htmlTemplateEngine: "njk",
   };
 };
